56bd8b9c018b75229ed70ebf240935a0fd1fa4a0
John Mendes <johnafmendes@gmail.com>
2021-05-16 16:10:34 +0100

    updating

--- src/unipesquisas/model/dao/DAO.java
+++ src/unipesquisas/model/dao/DAO.java
@@ -2,6 +2,7 @@ package unipesquisas.model.dao;
 
 import java.io.Serializable;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -24,8 +25,8 @@ public abstract class DAO implements Serializable {
 	/**
 	 * Referência à data source
 	 */
-	@Resource(name = "jdbc/unipesquisads")
-	private DataSource ds;
+	//@Resource(name = "jdbc/unipesquisads")
+	//private DataSource ds;
 	
 	/**
 	 * Objeto que representa a conexão com o banco de dados
@@ -36,16 +37,34 @@ public abstract class DAO implements Serializable {
 	 * Retorna a conexão aberta com o banco de dados
 	 * @return Conexão
 	 * @throws DAOException
+	 * @throws ClassNotFoundException 
 	 */
 	protected Connection getConnection() throws DAOException {
 		try {
 			if (conn == null) {
-				conn = ds.getConnection();
+				//conn = ds.getConnection();
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
+				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/unipesquisas","root","root");
+				if (conn != null) {
+	                System.out.println("Connected to the database!");
+	            } else {
+	                System.out.println("Failed to make connection!");
+	            }
 			}
 			return conn;
 		} catch (SQLException e) {
 			throw new DAOException(e);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		return conn;
 	}
 	
 	/**
